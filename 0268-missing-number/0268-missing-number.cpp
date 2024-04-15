29636f66f32e3bfcaf040380aeb127c9cfcb6e33
Pankaj Yadav <121039391+pankajyadav20@users.noreply.github.com>
2024-04-15 19:11:05 +0530

    Time: 16 ms (38.09%), Space: 20.3 MB (86.26%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int ans = nums.size();
-
-    for (int i = 0; i < nums.size(); i++)
-      ans = ans^ i ^ nums[i];
-return ans;
+    int missingNumber(vector<int>& arr) {
+        int n = arr.size();
+          int xorr1 = 0, xorr2 = 0;
+        
+        for(int i = 1;i<=n;i++) xorr1^=i;
+        for(int i = 0;i<n;i++) xorr2^=arr[i];
+        
+        cout<<xorr1<<" "<<xorr2<<endl;
+        
+        
+        return xorr1^xorr2;
     }
 };
